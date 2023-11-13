1ddeecde5564a2ec7354461a1d5dad5bd9fe7b46
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2023-11-12 21:36:52 -0700

    fix

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ export const oneAddress =
  * @returns: prepared string
  */
 const prepareString = (str) => {
-  const index = str.indexOf("\x00");
+  const index = str?.indexOf("\x00") ?? 0;
   if (index > 0) {
     return str.slice(0, str.indexOf("\x00"));
   } else {

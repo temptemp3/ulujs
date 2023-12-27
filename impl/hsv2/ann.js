cbc0a76fa0ae2bd4d6e6b73a304a30c3d2918bb2
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2023-12-27 13:43:59 -0700

    add decode, bump arccjs

--- impl/hsv2/ann.js
+++ impl/hsv2/ann.js
@@ -1,13 +1,75 @@
-import CONTRACT from "arccjs";
+import CONTRACT, { oneAddress } from "arccjs";
 
 import HSV2ANN from "../../abi/other/hsv2/ann.json" assert { type: "json" }; // hsv2 announcer
 
 /*
- * oneAddress is the address of the account that holds more
- * more than 0 ALGOs. This account is used to allow for simulation.
+ * decodeSymbol
+ * - decode symbol from hex
  */
-export const oneAddress =
-  "G3MSA75OZEJTCCENOJDLDJK7UD7E2K5DNC7FVHCNOV7E3I4DTXTOWDUIFQ";
+const decodeSymbol = (hex) => {
+  return Buffer.from(hex, "hex")
+    .filter((b) => b !== 0)
+    .toString("utf8");
+};
+
+/*
+ * decodeAnnounceEvent
+ * - decode announce event
+ */
+const decodeAnnounceEvent = (evt) => {
+  const [txid, round, timestamp, ...args] = evt;
+  const [address, opts, _] = args;
+  const [
+    ctcInfo,
+    startBlock,
+    endBlock,
+    rewardTokenId,
+    rewardsPerBlock,
+    stakedTokenId,
+    mPairTokenAId,
+    rPairTokenASymbol,
+    pairTokenBId,
+    rPairTokenBSymbol,
+    rewardTokenDecimals,
+    rRewardTokenSymbol,
+    stakedTokenDecimals,
+    stakedTokenPoolId,
+    rStakedTokenSymbol,
+    stakedTokenTotalSupply,
+  ] = opts;
+  let pairTokenAId;
+  if (mPairTokenAId[0] === "01") {
+    pairTokenAId = BigInt("0x" + mPairTokenAId[1]);
+  } else {
+    pairTokenAId = 0n;
+  }
+  const pairTokenASymbol = decodeSymbol(rPairTokenASymbol);
+  const pairTokenBSymbol = decodeSymbol(rPairTokenBSymbol);
+  const rewardTokenSymbol = decodeSymbol(rRewardTokenSymbol);
+  const stakedTokenSymbol = decodeSymbol(rStakedTokenSymbol);
+  return {
+    txid: txid,
+    round: round,
+    timestamp: timestamp,
+    address: address,
+    ctcInfo: ctcInfo,
+    startBlock: startBlock,
+    endBlock: endBlock,
+    rewardTokenId: rewardTokenId,
+    rewardsPerBlock: rewardsPerBlock,
+    stakedTokenId: stakedTokenId,
+    pairTokenAId: pairTokenAId,
+    pairTokenASymbol: pairTokenASymbol,
+    pairTokenBId: pairTokenBId,
+    pairTokenBSymbol: pairTokenBSymbol,
+    rewardTokenDecimals: rewardTokenDecimals,
+    rewardTokenSymbol: rewardTokenSymbol,
+    stakedTokenDecimals: stakedTokenDecimals,
+    stakedTokenPoolId: stakedTokenPoolId,
+    stakedTokenSymbol: stakedTokenSymbol,
+    stakedTokenTotalSupply: stakedTokenTotalSupply,
+  };
+};
 
 /*
  * Contract class
@@ -43,7 +105,10 @@ class Contract {
   getEvents = async (query) => await this.contractInstance.getEvents(query);
   Announce = async (query) => {
     const res = await this.contractInstance.Announce(query);
-    return handleResponse("Announce", res);
+    return {
+      ...res,
+      events: decodeAnnounceEvent(res.events),
+    };
   };
 }
 

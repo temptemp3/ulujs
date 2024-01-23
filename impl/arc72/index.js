14f951071174e07a4c6e16a454cb8884db794c40
David Runion <david.runion@imedemr.com>
2024-01-23 09:05:14 -0500

    uncommented prepareString, used by arc72_tokenURI

--- impl/arc72/index.js
+++ impl/arc72/index.js
@@ -10,14 +10,14 @@ import schema from "../../abi/arc72/index.js";
  * @param str: string to prepare
  * @returns: prepared string
  */
-// const prepareString = (str) => {
-//   const index = str.indexOf("\x00");
-//   if (index > 0) {
-//     return str.slice(0, str.indexOf("\x00"));
-//   } else {
-//     return str;
-//   }
-// };
+const prepareString = (str) => {
+  const index = str.indexOf("\x00");
+  if (index > 0) {
+    return str.slice(0, str.indexOf("\x00"));
+  } else {
+    return str;
+  }
+};
 
 /*
  * handleResponse

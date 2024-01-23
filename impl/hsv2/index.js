b54f9e69b4f191134d17d766f8b782ab7a9a8123
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2024-01-22 20:25:04 -0700

    fix export

--- impl/hsv2/index.js
+++ impl/hsv2/index.js
@@ -1,6 +1,6 @@
-import { ann } from "./ann.js";
-import { swap } from "./swap.js";
-import { tri } from "./tri.js";
+import ann from "./ann.js";
+import swap from "./swap.js";
+import tri from "./tri.js";
 export const hsv2 = {
   ann,
   swap,

88c860c955544863b98c3dfbeb54027f6706d064
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2024-01-26 08:48:26 -0700

    fix arc200 schema

--- abi/arc200/index.js
+++ abi/arc200/index.js
@@ -1,4 +1,8 @@
 import arc200Spec from "./contract.js";
 import arc200Extension from "./extension.js";
-const arc200 = { ...arc200Spec, ...arc200Extension };
-export default arc200;
+const arc200Schema = {
+  ...arc200Spec,
+  methods: [...arc200Spec.methods, ...arc200Extension.methods],
+  events: [...arc200Spec.events, ...arc200Extension.events],
+};
+export default arc200Schema;

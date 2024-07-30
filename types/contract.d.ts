0f796c20b5bd9a4e6b1e2b6d95a317d1f4d19def
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2024-07-30 15:31:21 -0600

    update contract type allowing any function

--- types/contract.d.ts
+++ types/contract.d.ts
@@ -1,4 +1,6 @@
 export class CONTRACT {
+  [key: string]: (...args: any[]) => any; // any function
+  // specific functions
   constructor(
     contractId: number,
     algodClient: any,
@@ -23,71 +25,4 @@ export class CONTRACT {
   setBeaconSelector: any;
   setOptins: any;
   setOnComplete: any;
-  // ARC72
-  arc72_approve: any;
-  arc72_ownerOf: any;
-  arc72_setApprovalForAll: any;
-  // mp205
-  v_sale_listingByIndex: any;
-  a_sale_listNet: any;
-  a_sale_buyNet: any;
-  a_sale_listSC: any;
-  a_sale_buySC: any;
-  a_sale_claimSC: any;
-  a_sale_deleteListing: any;
-  manager: any;
-  // arc200
-  arc200_approve: any;
-  arc200_transfer: any;
-  arc200_balanceOf: any;
-  arc200_allowance: any;
-  arc200_decimals: any;
-  hasBalance: any;
-  hasAllowance: any;
-  hasBox: any;
-  // nt200
-  createBalanceBox: any;
-  createAllowanceBox: any;
-  deposit: any;
-  withdraw: any;
-  // swap
-  Trader_swapAForB: any;
-  Trader_swapBForA: any;
-  Provider_withdrawA: any;
-  Provider_withdrawB: any;
-  Provider_withdraw: any;
-  Provider_depositA: any;
-  Provider_depositB: any;
-  Provider_deposit: any;
-  reserve: any;
-  SwapEvent: any;
-  // tri
-  Register: any;
-  // stakr
-  Pool: any;
-  Stake: any;
-  staked: any;
-  Info: any;
-  rewardsAvailable: any;
-  rewardsAvailableAt: any;
-  Staker_stake: any;
-  Staker_harvest: any;
-  Staker_withdraw: any;
-  Funder_deployPool: any;
-  // reach
-  _reachp_0: any;
-  // scs
-  setup: any;
-  configure: any;
-  fill: any;
-  participate: any;
-  close: any;
-  // channels
-  ChannelCreate: any;
-  a_channeler_useChannel: any;
-  // mechaswap
-  a_swap_execute: any;
-  a_swap_list: any;
-  e_swap_ListEvent: any;
-  v_swap_listingByIndex: any;
 }

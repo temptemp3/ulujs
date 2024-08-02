745f73cc88b6dc76e744bd4098e8b49ce4e88e91
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2024-08-01 23:52:50 -0600

    add nt200 methods

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.11.8] - 2024-08-01
+
+## Added
+
+- method for nt200 abi
+
 ## [0.11.7] - 2024-08-01
 
 ## Fixed
--- abi/nt200/index.js
+++ abi/nt200/index.js
@@ -38,13 +38,40 @@ const nt200Schema = {
       returns: {
         type: "byte",
       },
-    }
+    },
+    {
+      name: "register",
+      desc: "",
+      args: [
+        { name: "votekey", type: "byte[32]" },
+        { name: "selkey", type: "byte[32]" },
+        { name: "spkey", type: "byte[64]" },
+        { name: "votefst", type: "uint64" },
+        { name: "votelst", type: "uint64" },
+        { name: "votekd", type: "uint64" },
+      ],
+      returns: { type: "byte" },
+    },
+    {
+      name: "deregister",
+      desc: "",
+      args: [],
+      returns: { type: "byte" },
+    },
   ],
   events: [],
 };
 const arc200Schema = {
   ...nt200Schema,
-  methods: [...nt200Schema.methods, ...arc200Spec.methods, ...registerSchema.methods],
-  events: [...nt200Schema.events, ...arc200Spec.events, ...registerSchema.events]
+  methods: [
+    ...nt200Schema.methods,
+    ...arc200Spec.methods,
+    ...registerSchema.methods,
+  ],
+  events: [
+    ...nt200Schema.events,
+    ...arc200Spec.events,
+    ...registerSchema.events,
+  ],
 };
 export default arc200Schema;

52bc5dc30a94594a16e09a337953c4c62f5513f1
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2024-08-01 22:48:09 -0600

    fixed part key info object format

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.11.7] - 2024-08-01
+
+## Fixed
+
+- PartKeyInfoObject format
+
 ## [0.11.6] - 2024-08-01
 
 ## Fixed
--- utils/messenger.js
+++ utils/messenger.js
@@ -18,11 +18,11 @@ export const PartKeyInfoToObject = (pki) => {
     ts,
     who,
     address,
-    vote_k,
-    sel_k,
-    vote_fst,
-    vote_lst,
-    vote_kd,
-    sp_key,
+    vote_k: Buffer.from(vote_k, "hex").toString("base64"),
+    sel_k: Buffer.from(sel_k, "hex").toString("base64"),
+    vote_fst: Number(vote_fst),
+    vote_lst: Number(vote_lst),
+    vote_kd: Number(vote_kd),
+    sp_key: Buffer.from(sp_key, "hex").toString("base64"),
   };
 };
